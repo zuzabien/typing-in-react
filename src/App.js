d153d25b994119af6405d6ced1e707d749d7db64
Zuzanna Bienkowska <zuzanna.bienkowska@gmail.com>
2019-07-23 16:12:23 +0200

    Dodanie aplikacji

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// komponent klasowy musi zawsze zawierac metode render, funkcyjny wystarczy return
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      parText: ''
+    }
+  }
+
+  setParText = (event) => {
+    console.log(event.target.value);
+    // setState moze przyjac za parametr albo obiekt albo funkcje!!!!
+    this.setState({parText: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.parText}</p>
+        <textarea cols="30" rows="10" onChange={this.setParText}></textarea>
+      </div>
+    );
+  }
 }
 
 export default App;
